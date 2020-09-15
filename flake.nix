4e751796f8eba1a23237bbe902e7328bf8a54d37
Andrey Kuznetsov <fear@loathing.in>
2020-09-15 22:13:37 +0300

    don't use mozilla rust

--- flake.nix
+++ flake.nix
@@ -13,21 +13,6 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays =
-            let rustChannel = pkgs.rustChannelOf {
-              channel = "stable";
-              sha256 = "c522de65a51d139979f185f2bfbef61909a6be7d96e75c06370ad53fb778a9ea";
-            };
-            in [
-              (import "${nixpkgs-mozilla}/rust-overlay.nix")
-              (self: super:
-                {
-                  rustc = rustChannel.rust;
-                  inherit (rustChannel) cargo rust rust-fmt rust-std clippy;
-                  crate2nix = (import crate2nix { inherit pkgs; });
-                }
-              )
-            ];
         };
         cargoNix = pkgs.callPackage ./Cargo.nix { 
           inherit pkgs; 
